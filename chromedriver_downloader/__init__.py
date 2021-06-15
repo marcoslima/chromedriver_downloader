b9ea9bb2ee1bda6fccd58ea20693abce9b9f849a
Marcos Lima <marcos.j.s.lima@gmail.com>
2021-06-15 02:14:11 -0300

    Adicionado import em __init__

--- chromedriver_downloader/__init__.py
+++ chromedriver_downloader/__init__.py
@@ -3,3 +3,7 @@
 __author__ = """Marcos Lima"""
 __email__ = 'marcos.j.s.lima@gmail.com'
 __version__ = '0.1.0'
+
+from .chromedriver_downloader import ensure_chromedriver
+
+__all__ = ['ensure_chromedriver']
